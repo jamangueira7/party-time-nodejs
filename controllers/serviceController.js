f9d386755dc56d097afc37c6281939c60d08a8d4
João Mangueira <jamjs7@gmail.com>
2023-01-17 11:03:22 -0300

    routas de serviços configuradas

---
+++ controllers/serviceController.js
@@ -0,0 +1,7 @@
+const { Service: ServiceModel, serviceSchema} = require('../models/Service');
+
+const serviceController = {
+
+};
+
+module.exports = serviceController;
---
+++ routes/router.js
@@ -0,0 +1,8 @@
+const router = require('express').Router();
+
+//Services router
+const serviceRouter = require('./service');
+
+router.use('/', serviceRouter);
+
+module.exports = router;
